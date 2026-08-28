d26e6e556e808b0698c44f14381182ecf873302e
xinzhanhuang <306936840@qq.com>
2026-08-28 17:39:11 +0800

    refactor(comment): 增加删除云端鉴权

--- cloudfunctions/delete/index.js
+++ cloudfunctions/delete/index.js
@@ -1,148 +1,91 @@
-
 const cloud = require('wx-server-sdk')
 
-// 初始化 cloud
-cloud.init({
-  // API 调用都保持和云函数当前所在环境一致
-  env: cloud.DYNAMIC_CURRENT_ENV
-})
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV })
 
+const db = cloud.database()
 
-exports.main = async (event, context) => {
-//以下全部data内容
-  var liuyan=event._data.liuyan
-  var type111=event._data.type111
-  var id0=event._data.id0//lv0的id
-  var id1=event._data.id1//lv1,2的id
-  var time=event._data.time
-  var time1=event._data.time1
-  var id=event._data.id
-  var ku='ss'
-  if(liuyan){
-    var ku='tj'
+async function getActor(openid) {
+  const result = await db.collection('users').where({ _openid: openid }).limit(1).get()
+  return result.data[0]
+}
+
+async function isAdmin(actorId) {
+  const result = await db.collection('system').doc('system01').get()
+  const ids = result.data && result.data.system && result.data.system.glids
+  return Array.isArray(ids) && ids.includes(actorId)
+}
+
+exports.main = async (event = {}) => {
+  const data = event._data
+  const openid = cloud.getWXContext().OPENID
+  if (!openid) return { success: false, errCode: 'UNAUTHENTICATED' }
+  if (!data || typeof data.id !== 'string' || !data.id) {
+    return { success: false, errCode: 'INVALID_ARGUMENT' }
   }
-//以上全部data内容
-  var _=cloud.database().command
- 
 
-  if(type111=="tianmeizhoubian"){
-  if(id1==""){
-    //这是删除lv0评论
-    cloud.database().collection('tianmeizhoubian').doc(id).get().then((res)=>{
-      // console.log("打印取到的回复",res)
-      var ss_xx=res.data.ss_xx
-      var total=0
-      // console.log("total1：",total)
-      // console.log("chang:",ss_xx.huifunr.length)
-      if(liuyan==false){
-        for(var i=0;i<ss_xx.huifunr.length;i++){
-          var dd=ss_xx.huifunr[i]
-          if(dd.time==time && dd.plrid==id0){
-            // console.log("nbbb",dd.huifunb)
-  
-            if(dd.huifunb==undefined){
-              total=-1
-            }else{
-              total=dd.huifunb+1
-              total=-total
-            }
-            // console.log("total4：",total)
-            break
-          }
-        }
-      }
+  const actor = await getActor(openid)
+  if (!actor) return { success: false, errCode: 'USER_NOT_FOUND' }
 
-      // console.log("执行了删除lv0:total",total)
-      cloud.database().collection('tianmeizhoubian').doc(id).update({
-        data: {
-          'ss_xx.huifunr':_.pull({
-            'time':_.eq(time),
-            'plrid':_.eq(id0)
-          }),
-          'ss_xx.huifunb':_.inc(total)
-        }
+  const collectionName = data.liuyan
+    ? 'tj'
+    : (data.type111 === 'tianmeizhoubian' ? 'tianmeizhoubian' : 'ss')
+  const admin = await isAdmin(actor._id)
+
+  return db.runTransaction(async (transaction) => {
+    const postResult = await transaction.collection(collectionName).doc(data.id).get()
+    const post = postResult.data || {}
+    const detail = post.ss_xx || {}
+    const comments = Array.isArray(detail.huifunr) ? detail.huifunr.slice() : []
+    const parentIndex = comments.findIndex((comment) => {
+      return comment.plrid === data.id0 && comment.time == data.time
+    })
+    if (parentIndex < 0) {
+      return { success: false, errCode: 'COMMENT_NOT_FOUND' }
+    }
+
+    const parent = comments[parentIndex]
+    const deletingReply = data.id1 !== '' && data.id1 !== undefined && data.id1 !== null
+    let targetAuthorId = parent.plrid
+    let removedCount = 1
+
+    if (deletingReply) {
+      const replies = Array.isArray(parent.huifu) ? parent.huifu.slice() : []
+      const replyIndex = replies.findIndex((reply) => {
+        return reply.plrid === data.id1 && reply.time == data.time1
       })
-    })
-    
-  }else{
-    //这是删除lv1，2评论
-    // console.log("执行了删除lv1,2")
-    cloud.database().collection('tianmeizhoubian').where({
-      _id:id,
-      "ss_xx.huifunr.plrid":id0,
-      "ss_xx.huifunr.time":time,
-    }).update({
+      if (replyIndex < 0) return { success: false, errCode: 'REPLY_NOT_FOUND' }
+      targetAuthorId = replies[replyIndex].plrid
+      if (!admin && actor._id !== targetAuthorId && actor._id !== detail.lzid) {
+        return { success: false, errCode: 'PERMISSION_DENIED' }
+      }
+      replies.splice(replyIndex, 1)
+      parent.huifu = replies
+      parent.huifunb = Math.max(0, Number(parent.huifunb || 0) - 1)
+      comments[parentIndex] = parent
+    } else {
+      if (!admin && actor._id !== targetAuthorId && actor._id !== detail.lzid) {
+        return { success: false, errCode: 'PERMISSION_DENIED' }
+      }
+      removedCount += Number(parent.huifunb || 0)
+      comments.splice(parentIndex, 1)
+    }
+
+    const currentTotal = Number(detail.huifunb || 0)
+    const nextTotal = data.liuyan && !deletingReply
+      ? currentTotal
+      : Math.max(0, currentTotal - removedCount)
+    await transaction.collection(collectionName).doc(data.id).update({
       data: {
-        'ss_xx.huifunr.$.huifunb':_.inc(-1),
-        'ss_xx.huifunr.$.huifu':_.pull({
-          time:_.eq(time1),
-          plrid:_.eq(id1)
-        }),
-        'ss_xx.huifunb':_.inc(-1)
+        'ss_xx.huifunr': comments,
+        'ss_xx.huifunb': nextTotal
       }
     })
-  }
-} else {
-    if(id1==""){
-        //这是删除lv0评论
-        cloud.database().collection(ku).doc(id).get().then((res)=>{
-          // console.log("打印取到的回复",res)
-          var ss_xx=res.data.ss_xx
-          var total=0
-          // console.log("total1：",total)
-          // console.log("chang:",ss_xx.huifunr.length)
-          if(liuyan==false){
-            for(var i=0;i<ss_xx.huifunr.length;i++){
-              var dd=ss_xx.huifunr[i]
-              if(dd.time==time && dd.plrid==id0){
-                // console.log("nbbb",dd.huifunb)
-      
-                if(dd.huifunb==undefined){
-                  total=-1
-                }else{
-                  total=dd.huifunb+1
-                  total=-total
-                }
-                // console.log("total4：",total)
-                break
-              }
-            }
-          }
-    
-          console.log("执行了删除lv0:total",total)
-          cloud.database().collection(ku).doc(id).update({
-            data: {
-              'ss_xx.huifunr':_.pull({
-                'time':_.eq(time),
-                'plrid':_.eq(id0)
-              }),
-              'ss_xx.huifunb':_.inc(total)
-            }
-          })
-        })
-        
-      }else{
-        //这是删除lv1，2评论
-        // console.log("执行了删除lv1,2")
-        cloud.database().collection(ku).where({
-          _id:id,
-          "ss_xx.huifunr.plrid":id0,
-          "ss_xx.huifunr.time":time,
-        }).update({
-          data: {
-            'ss_xx.huifunr.$.huifunb':_.inc(-1),
-            'ss_xx.huifunr.$.huifu':_.pull({
-              time:_.eq(time1),
-              plrid:_.eq(id1)
-            }),
-            'ss_xx.huifunb':_.inc(-1)
-          }
-        })
-      }
 
-
-
+    return {
+      success: true,
+      deleted: deletingReply ? 'reply' : 'comment',
+      collection: collectionName,
+      removedCount
+    }
+  })
 }
-
-}
-
